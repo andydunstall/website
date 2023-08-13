546b204a2bfb9ae73c34f8b9c3a5925395d6fba5
Andy Dunstall <andydunstall@hotmail.co.uk>
2023-08-13 10:59:26 +0100

    app: add landing page

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+import Email from './icons/Email';
+import GitHub from './icons/GitHub';
+import Linkedin from './icons/Linkedin';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">
+        <h1>Andy Dunstall</h1>
+        <h2>Software Engineer | London</h2>
+        <div className="contact">
+          <a href="https://github.com/andydunstall" aria-label="GitHub" target="_blank" rel="noopener noreferrer"><GitHub /></a>
+          <a href="mailto:andydunstall@hotmail.co.uk" aria-label="Email"><Email /></a>
+          <a href="https://uk.linkedin.com/in/andy-dunstall-531b6122a" aria-label="Linkedin"><Linkedin /></a>
+        </div>
+      </div>
+	  
+	  <div className="landing">
+		<p>
+			Hi, I'm Andy Dunstall. I'm a software engineer based near London.
+
+			I've moved all of my notes <b><a href="https://publish.obsidian.md/andydunstall/public/About">here</a></b>.
+		</p>
+	  </div>
+    </div>
+  );
+}
+
+export default App;
